726f4cd62041903b89d0b6499d8f6ee9bc0884bf
Chris Whitworth <chris.whitworth@red-gate.com>
2014-07-25 12:21:36 +0100

    Mapping - first test

---
+++ Optional/Test/Mapping.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+namespace Optional
+{
+    public class Mapping
+    {
+        [Test]
+        public void MappingToPresentIsValid()
+        {
+            var a = Optional.Of(1);
+
+            var b = a.Map(i => i + 1);
+
+            Assert.That(b, Is.EqualTo(Optional.Of(2)));
+        }
+
+    }
+}
