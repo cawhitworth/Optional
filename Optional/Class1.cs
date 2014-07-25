c75c020c6fed45c0fdc8b553d325bd6a5ab2d90b
Chris Whitworth <chris.whitworth@red-gate.com>
2014-07-25 11:38:58 +0100

    Initial commit

---
+++ Optional/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Optional
+{
+    public class Tests
+    {
+        [Test]
+        public void CanConstructAnOptionalOfInt()
+        {
+            var a = Optional.Of(1);
+        }
+    }
+
+
+    public class Class1
+    {
+    }
+}
